121325e3e76addda6797f40cc887afc124872606
Stephen Toth <Stephentoth.st@gmail.com>
2018-04-26 14:20:13 -0400

    Create includes.php

---
+++ php/includes.php
@@ -0,0 +1,52 @@
+<?php
+include 'blogpost.php';
+include 'user.php';
+
+$mysqli = new mysqli("/*server*/", "/*mysql user*/", "/*mysql password for user*/", "/*database*/");
+
+
+function GetPosts($inId=null, $inTagId =null)
+{
+        global $mysqli;
+        if (!empty($inId))
+        {
+                $query = mysqli_query($mysqli, "SELECT * FROM posts WHERE id = " . $inId . " ORDER BY id DESC");
+        }
+        else if (!empty($inTagId))
+        {
+                $query = mysqli_query($mysqli, "SELECT posts.* FROM post_tags LEFT JOIN (posts) ON (post_tags.postID = posts.id) WHERE post_tags.tagID =" . $tagID . " ORDER BY posts.id DESC");
+        }
+        else
+        {
+                $query = mysqli_query($mysqli, "SELECT * FROM posts ORDER BY id DESC");
+        }
+
+        $postArray = array();
+        while ($row = mysqli_fetch_assoc($query))
+        {
+                $myPost = new blogpost($mysqli, $row["id"], $row['title'], $row['post_url'], $row['author_id'], $row['date_posted']);
+                array_push($postArray, $myPost);
+        }
+        return $postArray;
+}
+
+function GetPostFromUrl($inUrl)
+{
+	global $mysqli;
+	$query = mysqli_query($mysqli, "SELECT * FROM `posts` WHERE `post_url` LIKE '" . $inUrl . "'");
+	$row = mysqli_fetch_assoc($query);
+	$postInfo = new blogpost($mysqli, $row["id"], $row['title'], $row['post_url'], $row["author_id"], $row['date_posted']);
+	return $postInfo;
+}
+function GetUserFromId($inId){
+	global $mysqli;
+	$query = mysqli_query($mysqli, "SELECT * FROM `users` WHERE `id` = " . $inId );
+	$row = mysqli_fetch_assoc($query);
+	return new user($mysqli, $row['id'], $row['first_name'], $row['last_name'], $row['username'], $row['url'], $row['email'], $row['bio']);
+}
+
+function SendNote($inType, $inMessage){
+	echo "<div class='notification ". strtolower($inType) ."'><h2>". $inMessage ."</h2></div>";
+
+}
+?>
